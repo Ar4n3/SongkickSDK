9484b6b80db44fcc1d23ae962697a84d8b417a00
Enara <enaradelao@gmail.com>
2014-03-25 19:04:55 +0100

    Initial Commit

---
+++ SongkickSDK/SongkickSDK.h
@@ -0,0 +1,13 @@
+//
+//  SongkickSDK.h
+//  SongkickSDK
+//
+//  Created by Enara Lopez Otaegi on 25/03/14.
+//  Copyright (c) 2014 Enara Lopez Otaegi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SongkickSDK : NSObject
+
+@end
---
+++ SongkickSDK/SongkickSDK.m
@@ -0,0 +1,13 @@
+//
+//  SongkickSDK.m
+//  SongkickSDK
+//
+//  Created by Enara Lopez Otaegi on 25/03/14.
+//  Copyright (c) 2014 Enara Lopez Otaegi. All rights reserved.
+//
+
+#import "SongkickSDK.h"
+
+@implementation SongkickSDK
+
+@end
